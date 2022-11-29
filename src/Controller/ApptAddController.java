47e6f890a173d9acbd951f429aaf74bf3e0f71d4
Andrew Velasquez <theandrewjames@aol.com>
2022-11-29 16:39:44 -0700

    Start/end time combobox loaded with time  at 30 min intervals from 00:00 to 23:30

--- src/Controller/ApptAddController.java
+++ src/Controller/ApptAddController.java
@@ -2,6 +2,7 @@ package Controller;
 
 import Helper.DBContacts;
 import Model.Contacts;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
@@ -10,15 +11,31 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ResourceBundle;
+import java.util.TimeZone;
 
 public class ApptAddController implements Initializable {
+    public static int newApptId = 3;
     public ComboBox contactCombo;
+    public TextField apptIDTF;
+    public TextField apptTitleTF;
+    public TextField apptDescriptTF;
+    public TextField apptLocTF;
+    public TextField typeTF;
+    public DatePicker startDateDP;
+    public ComboBox startCB;
+    public ComboBox endCB;
+
 
     public void Back(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/View/apptView.fxml"));
@@ -28,6 +45,12 @@ public class ApptAddController implements Initializable {
         stage.show();
     }
 
+    /**
+     * Contact name combobox loaded with names from DB.Start/end time combobox loaded with time
+     * at 30 min intervals from 00:00 to 23:30
+     * @param url
+     * @param resourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
@@ -35,5 +58,28 @@ public class ApptAddController implements Initializable {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+        ObservableList<String> times = FXCollections.observableArrayList();
+        String [] halfHour = {"00", "30"};
+        for(int i = 0;i < 24;i++) {
+            for(int j =0;j<2;j++) {
+                String time = i + ":" + halfHour[j];
+                if(i < 10) {
+                    time = "0" + time;
+                }
+                times.add(time);
+            }
+        }
+        startCB.setItems(times);
+        endCB.setItems(times);
+    }
+
+    public void AddAppt(ActionEvent actionEvent) {
+    String title = apptTitleTF.getText();
+    String description = apptDescriptTF.getText();
+    String location = apptLocTF.getText();
+    String contactName = contactCombo.getValue().toString();
+    String type = typeTF.getText();
+    LocalDate startDate = startDateDP.getValue();
+
     }
 }
